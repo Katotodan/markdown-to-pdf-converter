eba7e0caf28ca82df53bca8dba1ed997956f01e1
DANIEL KATOTO <katotodan@gmail.com>
2025-11-23 17:54:17 +0300

    Loader added ant its functionalities

--- public/script.js
+++ public/script.js
@@ -1,13 +1,36 @@
 const form = document.getElementById("markdown-form");
 const downloadBtn = document.getElementById("download-btn");
+const loader = document.querySelector(".loader");
+const pdfViewer = document.getElementById('pdfViewer');
+const errorDisplayer = document.getElementById("error-displayer");
+const previewText = document.getElementById("preview");
 
 // Handle form submission
+const setError = (message) => {
+    errorDisplayer.textContent = message;
+    setTimeout(() => {
+        errorDisplayer.textContent = "";
+    }, 2000);
+    loader.style.display = "none";
+    pdfViewer.style.display = "none";
+    downloadBtn.style.display = "none";
+    previewText.style.display = "none";
+    return;
+};
+
 
 if (form) {
     form.addEventListener("submit", async (e) => {
         e.preventDefault();
+        loader.style.display = "block";
+        pdfViewer.style.display = "none";
+        downloadBtn.style.display = "none";
+        previewText.style.display = "none";
 
         const markdown = document.getElementById("markdown-input").value;
+        if (!markdown.trim()) {
+            return setError("Please enter some Markdown content.");
+        }
 
         try {
             const response = await fetch("/convert", {
@@ -16,7 +39,13 @@ if (form) {
                 body: JSON.stringify({ markdown }),
             });
 
-            if (!response.ok) throw new Error(`Server responded ${response.status}`);
+            if (!response.ok){
+                return setError("Failed to convert Markdown to PDF. Please try again.");
+            }
+            loader.style.display = "none";
+            pdfViewer.style.display = "block";
+            downloadBtn.style.display = "block";
+            previewText.style.display = "block";
 
             const blob = await response.blob();
             const objectUrl = URL.createObjectURL(blob);
