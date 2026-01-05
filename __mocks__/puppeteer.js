c058e37c5fff453de08e6123533f38d9e59dbf90
DANIEL KATOTO <katotodan@gmail.com>
2026-01-06 00:16:08 +0300

    Test Added

---
+++ __mocks__/puppeteer.js
@@ -0,0 +1,15 @@
+const pdfMock = Buffer.from("fake-pdf-content");
+
+const pageMock = {
+  setContent: jest.fn(),
+  pdf: jest.fn(() => Promise.resolve(pdfMock)),
+};
+
+const browserMock = {
+  newPage: jest.fn(() => Promise.resolve(pageMock)),
+  close: jest.fn(),
+};
+
+export default {
+  launch: jest.fn(() => Promise.resolve(browserMock)),
+};
